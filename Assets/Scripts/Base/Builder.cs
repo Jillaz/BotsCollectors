8e9a493dcd5819a859cb73e0c5ae543ea6c4d9ce
Jillaz <pingwin1111@ya.ru>
2026-05-10 14:40:18 +0300

    ObjectSelector Added

--- Assets/Scripts/Base/Builder.cs
+++ Assets/Scripts/Base/Builder.cs
@@ -7,69 +7,39 @@ public class Builder : MonoBehaviour
     [SerializeField] private Counter _counter;
     [SerializeField] private BuildingOptions _buildingOptions;
     [SerializeField] private BotsTaskManager _botsTaskManager;
-    [SerializeField] private ParticleSystem _particleSystem;
-    [SerializeField] private Raycaster _playerInputController;
-    [SerializeField] private Flag _buildFlag;
-    private bool _isBuildBase = false;
-    private bool _isSelected = false;
     private int _minBotsNumberToBuildBase = 2;
     private Vector3 _buildLocation;
+
+    public bool IsBuildBase { get; private set; } = false;
+    public Vector3 GetBuildLocation => _buildLocation;
 
     private void Start()
     {
         _counter.ValueChanged += Build;
-        _playerInputController.MouseClicked += MouseClicked;
+        _buildLocation = transform.position;
     }
 
     private void OnDisable()
     {
         _counter.ValueChanged -= Build;
-        _playerInputController.MouseClicked -= MouseClicked;
     }
 
-    private void SelectBase()
+    public void SetBuildPosition(Vector3 buildLocation)
     {
-        _isSelected = true;
-        _particleSystem.Play();
+        _buildLocation = buildLocation;
+        IsBuildBase = true;
     }
 
-    private void DeselectBase()
-    {
-        _isSelected = false;
-        _particleSystem.Stop();
-    }
-
-    private void MouseClicked(RaycastHit hit)
-    {
-        float defaultY = 1f;
-
-        if (hit.collider.gameObject == this.gameObject)
-        {
-            SelectBase();
-        }
-        else if (hit.collider.GetComponent<Builder>())
-        {
-            DeselectBase();
-        }
-        else if (_isSelected)
-        {
-            _buildLocation = new Vector3(hit.point.x, defaultY, hit.point.z);
-            _isBuildBase = true;
-            PlaceBuildFlag();
-        }
-    }
 
     private void Build(int value)
     {
-        if (_isBuildBase && _botsList.Count() >= _minBotsNumberToBuildBase)
+        if (IsBuildBase && _botsList.Count() >= _minBotsNumberToBuildBase)
         {
             if (value >= _buildingOptions.GetBasePrice())
             {
                 _botsTaskManager.BuildBase(_buildLocation, _buildingOptions.GetBase());
                 _counter.ReduceValue(value);
-                _isBuildBase = false;
-                DeselectBase();
-                ReturnBuildFlag();
+                IsBuildBase = false;
             }
         }
         else
@@ -81,17 +51,5 @@ public class Builder : MonoBehaviour
                 _counter.ReduceValue(value);
             }
         }
-    }
-
-    private void PlaceBuildFlag()
-    {
-        _buildFlag.gameObject.SetActive(true);
-        _buildFlag.transform.position = _buildLocation;
-    }
-
-    private void ReturnBuildFlag()
-    {
-        _buildFlag.gameObject.SetActive(false);
-        _buildFlag.transform.position = transform.position;
     }
 }
--- Assets/Scripts/ObjectSelector.cs
+++ Assets/Scripts/ObjectSelector.cs
@@ -1,16 +1,57 @@
+using TMPro;
 using UnityEngine;
 
 public class ObjectSelector : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private Raycaster _raycaster;
+    [SerializeField] private ParticleSystem _particleSystem;
+    [SerializeField] Flag _flag;
+    private Builder _builder = null;
+
+    private void OnEnable()
     {
-        
+        _raycaster.MouseClicked += MouseClicked;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        _raycaster.MouseClicked -= MouseClicked;
+        _particleSystem.Stop();
+    }
+
+    private void Start()
+    {
+        _flag.gameObject.SetActive(false);
+    }
+
+    private void MouseClicked(RaycastHit hit)
+    {
+        if (hit.collider.GetComponent<Builder>())
+        {
+            _builder = hit.collider.GetComponent<Builder>();
+            Select();
+        }
+        else if (_builder != null)
+        {
+            SetBuildLocation(hit);
+        }
+    }
+
+    private void Select()
+    {
+        _particleSystem.transform.position = _builder.transform.position;
+        _particleSystem.Play();
+
+        _flag.transform.position = _builder.GetBuildLocation;
+        _flag.gameObject.SetActive(_builder.IsBuildBase);
+    }
+
+    private void SetBuildLocation(RaycastHit hit)
+    {
+        float defaultY = 1f;
+
+        _flag.transform.position = new Vector3(hit.point.x, defaultY, hit.point.z);
+        _builder.SetBuildPosition(_flag.transform.position);
+        _flag.gameObject.SetActive(_builder.IsBuildBase);
     }
 }
